569d604282570115e5c6febd64169aba4805fce5
Gabriel Kanashiro <gkana@AC-LPT-00727.local>
2023-12-05 21:35:20 -0300

    finished day #1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kanashir0/go-exchange
+
+go 1.21.5
+
+require github.com/google/uuid v1.4.0
---
+++ internal/market/entity/transaction.go
@@ -0,0 +1,30 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
+	ID           string
+	SellingOrder *Order
+	BuyingOrder  *Order
+	Shares       int
+	Price        float64
+	Total        float64
+	DateTime     time.Time
+}
+
+func NewTransaction(SellingOrder *Order, buyingOrder *Order, shares int, price float64) *Transaction {
+	total := float64(shares) * price
+	return &Transaction{
+		ID:           uuid.New().String(),
+		SellingOrder: SellingOrder,
+		BuyingOrder:  buyingOrder,
+		Shares:       shares,
+		Price:        price,
+		Total:        total,
+		DateTime:     time.Now(),
+	}
+}
